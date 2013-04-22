320ed0005214cdfb6ccab9255d432c8e31be3db4
chetsteadman <slayman@ontraport>
2013-04-22 16:36:12 -0700

    object creation

--- bounceparse.php
+++ bounceparse.php
@@ -18,49 +18,52 @@ $dbPass = 'password1';
 $dbName = 'pmta_parser';
 $dbTable = 'bounces';
 
+
+//Working directory for logs
+//$dir = '/var/backups/UnitedLayer_ARTICHOKE/pmta/logs/';
+//$dir = '/home/slayman/bounceparser/logs/';
+$dir = '/home/steve/Documents/Temp/';
+
 class PMTA_PARSER {
     public $csv;
+    var $link = mysqli_connect($dbServer, $dbUser, $dbPass, $dbName);
+    var $dataArray;
 
     function __construct ($csv){
         $this->csv = $csv;
     }
 
     function parse() {
-        foreach ($logFiles as $i) {
-            if ($handle = fopen($i, 'r')) {
-                $currentRow = 1;
-                while ($data = fgetcsv($handle, ",")) {
-                    $number_of_fields = count($data);
-                    for ($c=0; $c < $number_of_fields; $c++) {
-                        $dataArray[$currentRow] = $data;
-                    }
-                    $currentRow ++;
+        if ($handle = fopen($this->csv, 'r')) {
+            $currentRow = 1;
+            while ($data = fgetcsv($handle, ",")) {
+                $number_of_fields = count($data);
+                for ($c=0; $c < $number_of_fields; $c++) {
+                    $this->$dataArray[$currentRow] = $data;
                 }
+                $currentRow ++;
             }
         }
         fclose($handle);
     }
 
     function store(){
-        $link = mysqli_connect($dbServer, $dbUser, $dbPass, $dbName);
-
-        foreach ($dataArray as $item){
+        foreach ($this->dataArray as $item){
             if ($item[0] == 'b') {
                     //break up X-MRID field and convert array vals to integer
                     $msgInfo = array_map('intval', explode('.',$item[23]));
                     $deliv = strtotime($item[1]);
                     $queued = strtotime($item[2]);
-                    mysqli_query($link, "INSERT INTO bounces (delivered, queued, recipient, dsnstatus, bouncereason, acct, contact, msgid, seqid) VALUES ('$deliv','$queued','$item[4]','$item[7]','$item[8]','$msgInfo[1]','$msgInfo[2]','$msgInfo[4]','$msgInfo[5]')");
+                    mysqli_query($this->link, "INSERT INTO bounces (delivered, queued, recipient, dsnstatus, bouncereason, acct, contact, msgid, seqid) VALUES ('$deliv','$queued','$item[4]','$item[7]','$item[8]','$msgInfo[1]','$msgInfo[2]','$msgInfo[4]','$msgInfo[5]')");
             };
         };
     }
-//mysqli_query($link, 'DELETE FROM '.$dbTable.' WHERE delivered<'.time()-2592000);
+    function clearup(){
+        mysqli_query($this->link, 'DELETE FROM '.$dbTable.' WHERE delivered<'.time()-2592000);
+    }
 }
 
-//Working directory for logs
-//$dir = '/var/backups/UnitedLayer_ARTICHOKE/pmta/logs/';
-//$dir = '/home/slayman/bounceparser/logs/';
-$dir = '/home/steve/Documents/Temp/';
+exec('rm -rf '.$dir.'/temp/*');
 
 //Current days' archives
 $tars = glob($dir.'old_logs_'.date('Ymd').'*');
@@ -70,3 +73,10 @@ foreach ($tars as $i) {
 }
 
 $logFiles = glob($dir.'temp/old_logs/*');
+
+foreach ($logfiles as $csv) {
+    $bounceLog = new PMTA_PARSER($csv);
+    $bounceLog->parse();
+    $bounceLog->store();
+    $bounceLog->cleanup();
+}
