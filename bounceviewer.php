b3c6dc42e695f2c53422a4a4b2ebfeb68c8d4cf2
chetsteadman <slayman@ontraport>
2013-05-21 08:54:45 -0700

    added viewer

---
+++ bounceviewer.php
@@ -0,0 +1,14 @@
+<?php
+
+/*****
+* Bounce Parser
+* 
+* A utility to view bounces
+* 
+* @author       Steve Layman
+* @copyright    Copyright (c) 2011, Ontraport, LLC
+*/
+
+// ----------------------------------------------------------------------------------------------
+
+include "./config.inc.php";
